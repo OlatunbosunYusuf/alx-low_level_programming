d65b7e2d946277cd638f32645b7ccd7e80a4afa3
OlatunbosunYusuf <olatunbosun.y@yahoo.com>
2022-09-08 15:24:04 -0700

    My 4-puts.c  commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
 #!/bin/bash
-gcc puts "Programming is like building a multilingual puzzle"
+gcc
+#include <stdio.h>
+/* main- Serves as the entry point for the program */
+/* Return: By default returns zero if no error otherwise return non-zero value.*/
+ int main (void)
+{
+	puts ("Programming is like building a multilingual puzzle");
+	return (0);
+}
