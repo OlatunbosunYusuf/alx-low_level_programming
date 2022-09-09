a5400b57d1a3542c17646fe7b599048135a0dd15
OlatunbosunYusuf <olatunbosun.y@yahoo.com>
2022-09-08 18:07:26 -0700

    My 5-printf.c commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- *main-Serves as the entry point for the program 
- *Return:By default returns zero if no errors
- */
+*main-Serves as the entry point for the program 
+*Return:By default returns zero if no errors
+*/
 int main(void)
 {
- printf("with proper grammar, but the outcome is a piece of art,\n");
- return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
