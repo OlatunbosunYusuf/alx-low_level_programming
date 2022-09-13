2005d8c04537595d70372fe11ac3a18c648ba8ec
OlatunbosunYusuf <olatunbosun.y@yahoo.com>
2022-09-13 15:23:23 -0700

    My 0-putchar.c  commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+/**
+* Main-the entry point to the program
+* local variable definition
+*/
+int main(void)
+{
+int a = 0;
+
+_putchar(":%d\n);
+return 0;
+
+}
+
+
