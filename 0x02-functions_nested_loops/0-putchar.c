7dbba02ec86f8a3bcc7dc8f6d0909a5241cc73c6
OlatunbosunYusuf <olatunbosun.y@yahoo.com>
2022-09-14 11:44:40 -0700

    My 0-putchar.c commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
-#include"main.h"
+#include <main.h>
 /**
 * Main-the entry point to the program
 * local variable definition,prints _putchar followed by a new line
-* Return:0
+* Reture: 0
 */
-extern int "_putchar"; 
-
 int main(void)
 {
+	char text[10] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+{
+_putchar(text[i]);
+}
 putchar('\n');
+
 return (0);
 }
 
