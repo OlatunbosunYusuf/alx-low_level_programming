aac30cba956951dc2244d1a4ecaea68f562a0c8d
OlatunbosunYusuf <olatunbosun.y@yahoo.com>
2022-09-15 04:43:40 -0700

    My 1-alphabet.c commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,19 @@
-#include "stdio.h"
+#include "unistd.h"
 /**
 *Print_alphabet- function that prints the alphabet, lowercase
 *You can only use _putchar twice in your code
 *Return:nothing
 */
-void print_alphabet(void)
+void print_alphabet(void);
 {
-clrscr();
+char letter
+letter = 'a'; 
+while (letter <= 'z';)
+{ 
+_putchar (letter);
+letter++;
+}
+_putchar ('\n');
 
-int ch;
-for (ch= 'a'; ch<= 'z'; ch++)
-
-putchar (ch);
-
-putchar ('\n');
+return (0);
 }
