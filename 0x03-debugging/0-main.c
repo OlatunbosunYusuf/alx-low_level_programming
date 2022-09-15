a7275305504162028f8fabcc6d779350dc5ab70b
OlatunbosunYusuf <olatunbosun.y@yahoo.com>
2022-09-15 11:06:05 -0700

    My 0-main.c commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* main - print positive or negative integer
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 int main(void)
 {
 int i;
 i = 98;
-positive_or_negativei(i);
+positive_or_negative(i);
 return (0);
 }
