c9ceef71d33d98bb3745d43b6648fac64ada396a
OlatunbosunYusuf <olatunbosun.y@yahoo.com>
2022-09-15 10:43:13 -0700

    My  0-main.c commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,23 +1,13 @@
 #include "main.h"
 
 /**
-
 * main - print positive or negative integer
-
 * Return: 0
-
 */
-
 int main(void)
-
 {
-
 int i;
-
 i = 0;
-
 positive_or_negativei(i);
-
 return (0);
-
 }
