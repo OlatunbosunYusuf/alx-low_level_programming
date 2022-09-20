5db5da08288c0823e9e6c0f2e6864c16cad194e7
OlatunbosunYusuf <olatunbosun.y@yahoo.com>
2022-09-19 18:02:32 -0700

    My 2-strlen.c commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,13 @@
 #include"main.h"
 /**
- * _strlen - Returns the length of a string 
+ * _strlen -returns the length of a string 
  * @s: String to count
  *
  * Return: String length
  */
 int _strlen(char *s)
 {
-int c = 0;
+int c = 0;i
 
 for (; *s != '\0'; s++)
 {
