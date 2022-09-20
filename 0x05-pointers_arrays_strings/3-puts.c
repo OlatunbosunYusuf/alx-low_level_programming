d7ea69b616e66ebe9261a0d9b8c1d017aba887ee
OlatunbosunYusuf <olatunbosun.y@yahoo.com>
2022-09-19 18:26:15 -0700

    My 3-puts.c commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *_puts - Prints a string
- * @str: The string to print  
+ * _puts - Prints a string
+ * @str: The string to print
  *
  * Return: void
- */
+*/
 void _puts(char *str)
 {
 for (; *str != '\0'; str++)
