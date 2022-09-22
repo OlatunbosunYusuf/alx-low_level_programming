039e75c53155d22fbb52a03b2e0542502d547da2
OlatunbosunYusuf <olatunbosun.y@yahoo.com>
2022-09-22 10:48:16 -0700

    My 1-strncat.c commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,14 +11,13 @@ char *_strcat(char *dest, char *src, int n)
 {
 int i;
 int j;
-
 i = 0;
 while (dest[i] != '\0')
 {
-i++;
+i++;i
 }
 j = 0;
-while (j < n && src [j] != '\0')
+while (j < n && src[j] != '\0')
 {
 dest[i] = src[j];
 i++;
