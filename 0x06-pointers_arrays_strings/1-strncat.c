4e9562d81271318451f76ca02061771f4173bed9
OlatunbosunYusuf <olatunbosun.y@yahoo.com>
2022-09-22 10:44:33 -0700

    My 1-strncat.c commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - Concatenates two strings
+ * @dest: Destination
+ * @src: source
+ *
+ * @n: Amount of bytes used from src
+ * Return: The pointer to dest
+ */
+char *_strcat(char *dest, char *src, int n)
+{
+int i;
+int j;
+
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (j < n && src [j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}
